5a502932ad21d3de428fd31b45f0ae2d50190009
cmcfarland <cmcfarland@mindharbor.com>
2021-06-18 22:23:17 -0400

    Update 4025.js

--- 4025.js
+++ 4025.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     if(!window.location.toString().includes("3d-printers")) 
-    {
-	window.location.href = "https://homers3dprinter.com/collections/3d-printers";      
+    {	
+	console.log("----eDesk-----");
+	console.log(new Date());  	
     }
 }
